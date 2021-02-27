02b0bc562a91212199eff179bdcfa4de02ad677f
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-02-27 23:43:58 +1400

    Refactor - program1 functionality added

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,5 +1,14 @@
-public class Tictactoe{
-    public static void main(String[] args){
-        System.out.println("Welcome to Tictactoe");
+public class Tictactoe {
+	static char[] board = new char[10];
+    public char[] createBoard() {
+         for(int i=0; i<9; i++) {
+            board[i] = '  ';
+         }
+         return board;
+    }
+    public static void main(String[] args) {
+        Tictactoe tictactoe = new Tictactoe();
+        tictactoe.createBoard();
     }
 }
+
